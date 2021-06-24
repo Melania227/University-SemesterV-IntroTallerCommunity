bf39975751d5f8b24fe720b862699a274eff4dc7
Paolanc <apaolachsanchez@gmail.com>
2021-06-24 09:30:28 -0600

    C: Firebase

--- IntroTaller_WebApp/src/app/app.component.ts
+++ IntroTaller_WebApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebApp';
+  rootRef: firebase.database.Reference;
+
+  constructor(private angularFirebaseDatabase: AngularFireDatabase){
+    
+    const database = this.angularFirebaseDatabase.database;
+    this.rootRef = database.ref('/Ejercicios/');
+  }
+
+
+  getUserList() { 
+    this.rootRef.once('value', function(snapshot) {
+    
+      snapshot.forEach(function(childSnapshot) {
+        const data = snapshot.val();
+
+        console.log(data);
+          });
+    });
+    
+  }
+
 }
--- IntroTaller_WebApp/src/app/app.module.ts
+++ IntroTaller_WebApp/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
