911821aeeab9a2e684857212c0bcd27e797e134b
Melania Morales <melaniacrc@gmail.com>
2021-06-29 09:44:12 -0600

    U: Exercises List - Students View

--- IntroTaller_WebApp/src/app/components/exercises-list/exercises-list.component.ts
+++ IntroTaller_WebApp/src/app/components/exercises-list/exercises-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ejercicio } from 'src/app/models/ejercicio.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-exercises-list',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExercisesListComponent implements OnInit {
   prueba=[1,4,6.6,8,98,6,5,4,3,342,3,456,73,3];
-  calification = [true, true, false, false, false];
-  constructor() { }
+  calification = [false, false, false, false, false];
+  exercises:Ejercicio[];
+
+  constructor(
+    public firebase: FirebaseService
+  ) { }
 
   ngOnInit(): void {
+    (this.firebase.getAllExercises().then((data) => this.exercises = data));
+  }
+
+  getStars(exercise: Ejercicio){
+    for (let index = 0; index < exercise.level; index++) {
+      this.calification[index] = true;
+    }
+    return this.calification;
   }
 
 }
